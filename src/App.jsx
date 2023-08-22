a07c5a7c019a0b01e895b8348937ad4978dbf7a7
Alex Sandro <alex.sandro.1199@gmail.com>
2023-08-22 14:53:01 -0300

    componentes criados

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Produtos from "./Components/Produtos";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Produtos />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
