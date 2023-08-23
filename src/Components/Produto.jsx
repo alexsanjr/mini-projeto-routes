3a8947171506b5a3394f7627afea63414c1762fe
Alex Sandro <alex.sandro.1199@gmail.com>
2023-08-23 10:44:50 -0300

    fetch e componente Produto Criado

---
+++ src/Components/Produto.jsx
@@ -0,0 +1,11 @@
+import styles from "./Produto.module.css";
+
+const Produto = () => {
+  return (
+    <div className={styles.produto}>
+      <h1>Produto</h1>
+    </div>
+  );
+};
+
+export default Produto;
